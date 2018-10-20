8d7cb08042adc02e5dcf08fa67c5ae9afdc17f4a
vivekarul <43576663+vivekarul@users.noreply.github.com>
2018-10-20 10:55:24 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+    city:String ="Chennai";
+    
+    mobile:number=1122;
+
+    data:any =["Raja","Guhan", "Anbu", "Kamal",78678,"kesav"];//array
+    //for stores more than one values
+
+
+
+
+
+
+
+}
