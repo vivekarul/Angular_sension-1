df218f116507d611e90c46d78c528bd910caba5a
vivekarul <43576663+vivekarul@users.noreply.github.com>
2018-10-29 18:30:05 +0530

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -7,23 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
-   myjson:any ={name:"Guhan",age:27} //object
-
-  college:any ={    //object
-              sem1:[
-                       {name:"Raja",tamil:56,eng:67},
-                       {name:"anbu",tamil:67,eng:87}
-
-              ],
-              sem2:[
-                {name:"Rohit",tamil:56,eng:67},
-                {name:"mani",tamil:67,eng:87}
-              ]
-   }
-
-
-
-
 
 
 }
--- app.module.ts
+++ app.module.ts
@@ -1,15 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { DeptComponent } from './dept/dept.component';
+import { LibComponent } from './lib/lib.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    DeptComponent,
+    LibComponent
   ],
   imports: [
-    BrowserModule,FormsModule
+    BrowserModule,FormsModule,RouterModule.forRoot(
+      [
+        {path:"url1",component:AboutComponent},
+        {path:"url2",component:DeptComponent},
+        {path:"url3",component:LibComponent}
+      ]
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
