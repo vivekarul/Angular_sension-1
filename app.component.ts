22c70818e15a8d716188e89a770076e4add4380a
vivekarul <43576663+vivekarul@users.noreply.github.com>
2018-10-29 18:30:46 +0530

    Add files via upload

--- app.component.ts
+++ app.component.ts
@@ -7,6 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+   search:any={name:"", tamil:"", eng:""}
 
+  college:any ={    
+    sem1:[
+             {name:"Raja",tamil:56,eng:67},
+             {name:"Anbu",tamil:67,eng:87},
+             {name:"Rajesh",tamil:86,eng:54},
+             {name:"Vimal",tamil:67,eng:34}
 
+    ],
+    sem2:[
+      {name:"Rohit",tamil:56,eng:67},
+      {name:"mani",tamil:67,eng:87}
+    ]
+  }
+  //https://www.npmjs.com/package/ngx-filter-pipe
 }
--- app.module.ts
+++ app.module.ts
@@ -1,28 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {RouterModule} from '@angular/router';
+import {FilterPipeModule} from  'ngx-filter-pipe';
+
 
 import { AppComponent } from './app.component';
-import { AboutComponent } from './about/about.component';
-import { DeptComponent } from './dept/dept.component';
-import { LibComponent } from './lib/lib.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    DeptComponent,
-    LibComponent
+    
   ],
   imports: [
-    BrowserModule,FormsModule,RouterModule.forRoot(
-      [
-        {path:"url1",component:AboutComponent},
-        {path:"url2",component:DeptComponent},
-        {path:"url3",component:LibComponent}
-      ]
-    )
+    BrowserModule,FormsModule, FilterPipeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
